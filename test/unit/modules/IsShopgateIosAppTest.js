fa224f48f4cd45ea3f266a2444859594a1f19535
Oliver Müller <oliver.mueller@shopgate.com>
2018-02-09 08:48:10 +0100

    BIGC-628: added test for IsShopgateJSBridgeAvailable

--- test/unit/modules/IsShopgateIosAppTest.js
+++ test/unit/modules/IsShopgateIosAppTest.js
@@ -4,7 +4,7 @@ import { isShopgateIosApp } from '../../../src/modules/IsShopgateIosApp';
 const { expect } = chai;
 
 describe('IsShopgateIosApp', () => {
-  it('should identify an IOS browser', () => {
+  it('should identify an IOS device', () => {
     global.navigator = {
       userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_1_2 like Mac OS X) AppleWebKit/604.3.5 (KHTML, like Gecko) Mobile/15B202 libshopgate/16.0 (Shopgate Standalone 10.16.0 Codebase:10.16.0)',
     };
@@ -12,7 +12,7 @@ describe('IsShopgateIosApp', () => {
     expect(isShopgateIosApp()).equal(true);
   });
 
-  it('should not identify an IOS browser', () => {
+  it('should not identify an IOS device when its actually an Android device', () => {
     global.navigator = {
       userAgent: '5.0 (Linux; Android 7.1.1; XT1650 Build/NPLS26.118-20-5-11; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/64.0.3282.137 Mobile Safari/537.36',
     };
