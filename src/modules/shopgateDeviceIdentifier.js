1d0972cf1a6f671a168263dc795b86ace8d17034
Oliver Müller <oliver.mueller@shopgate.com>
2018-02-14 13:58:32 +0100

    BIGC-628: merged two identify methods for Android and iOS in one file: shopgateDeviceIdentifier

--- src/modules/shopgateDeviceIdentifier.js
+++ src/modules/shopgateDeviceIdentifier.js
@@ -6,3 +6,12 @@
 export function isShopgateAndroidApp() {
   return !!window.SGJavascriptBridge && navigator.userAgent.indexOf('libshopgate') === -1;
 }
+
+/**
+ * Determines if the current browser is a Shopgate App or not
+ *
+ * @returns {boolean}
+ */
+export function isShopgateIosApp() {
+  return navigator.userAgent.indexOf('libshopgate') !== -1;
+}
